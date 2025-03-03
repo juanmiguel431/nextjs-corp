bf4bebddc84c04fd583121a8b1c0d27a06d7a9b2
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-02 22:28:18 -0400

    Header improve with tailwind css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,15 @@ import Link from 'next/link';
 
 export default function Header () {
   return (
-    <ul style={{ display: 'flex', gridGap: 10 }}>
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/performance">Performance</Link></li>
-      <li><Link href="/reliability">Reliability</Link></li>
-      <li><Link href="/scale">Scale</Link></li>
-    </ul>
+    <div className="w-full absolute text-white z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+        <Link href="/" className="font-bold text-3xl">Home</Link>
+        <div className="space-x-4 text-xl">
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </div>
+      </nav>
+    </div>
   )
 }
