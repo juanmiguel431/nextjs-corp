a546ca6be7896286d17b41c36c11cdf795ba4751
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-02 21:50:29 -0400

    Adding public images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src="/home.jpg"
+          alt="Care Factory"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
     </div>
   );
 }
