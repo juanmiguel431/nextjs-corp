7b05926ab5e3e449cd55a58650ac146f53db7652
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-03-02 20:52:00 -0400

    App Router

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Performance() {
+  return (
+    <div>
+      <h1>Performance</h1>
+      <br/>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Reliability() {
+  return (
+    <div>
+      <h1>Reliability</h1>
+      <br/>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
